0b7223e308d561fe69ed52a5f63eb10cd92e5aa7
Thomas Grimonet <thomas.grimonet@gmail.com>
2023-11-11 13:33:15 +0100

    Update dummy.py

--- anta_custom/dummy.py
+++ anta_custom/dummy.py
@@ -2,6 +2,6 @@
 Test functions related to system-level features and protocols
 """
 
-## Please refer to https://github.com/titom73/atd-anta-demo/blob/main/docs/demo-tests.md for complete description
+## Please refer to https://github.com/titom73/anta-malaga/blob/main/docs/demo-tests.md for complete description
 
 from typing import Optional
